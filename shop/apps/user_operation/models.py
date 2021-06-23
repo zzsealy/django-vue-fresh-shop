8e0f619672e03475233e93ab966d837fa6ec5803
sealy <1147575371@qq.com>
2021-06-23 11:31:16 +0800

    ADD: add the model user operation

--- shop/apps/user_operation/models.py
+++ shop/apps/user_operation/models.py
@@ -17,7 +17,7 @@ class UserFav(models.Model):
     add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
 
     class Meta:
-        verbose_name = "用户收藏"
+        verbose_name = '用户收藏'
         verbose_name_plural = verbose_name
 
     def __str__(self):
@@ -30,33 +30,44 @@ class UserLeavingMessage(models.Model):
     '''
 
     MESSAGE_CHOICES = (
-        (1, "留言"),
+        (1, '留言'),
         (2, '投诉'),
         (3, '咨询'),
         (4, '售后'),
         (5, '求购'),
     )
 
-    user = models.ForeignKey(User, verbose_name="用户", on_delete=CASCADE)
-    msg_type = models.IntegerField(default=1, choices=MESSAGE_CHOICES, verbose_name="留言类型",
+    user = models.ForeignKey(User, verbose_name='用户', on_delete=CASCADE)
+    msg_type = models.IntegerField(default=1, choices=MESSAGE_CHOICES, verbose_name='留言类型',
                                    help_text='留言类型:1(留言), 2(投诉), 3(询问), 4(售后), 5(求购)')
-    subject = models.CharField(max_length=100, default='', verbose_name="主题")
+    subject = models.CharField(max_length=100, default='', verbose_name='主题')
     message = models.TextField(
         default='',  verbose_name='留言内容', help_text='留言内容')
     file = models.FileField(verbose_name='上传的文件', help_text='上传的文件')
     add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
 
     class Meta:
-        verbose_name = "用户留言"
+        verbose_name = '用户留言'
         verbose_name_plural = verbose_name
 
     def __str__(self):
-        return "%s: %s" % (self.user.name, self.subject)
+        return '%s: %s' % (self.user.name, self.subject)
 
 
 class UserAddress(models.Model):
     '''
     用户收获地址
     '''
-    user = models.ForeignKey(User, on_delete=CASCADE)
-    
+    user = models.ForeignKey(User, verbose_name='用户', on_delete=CASCADE)
+    district = models.CharField(max_length=100, default='', verbose_name='区域')
+    address = models.CharField(max_length=100, default='', verbose_name='详细地址')
+    singer_name = models.CharField(max_length=50, default='', verbose_name='收货人姓名')
+    singer_mobile = models.CharField(max_length=11, default='', verbose_name='收货人手机号')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '收货地址'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '%s: %s' % (self.user.name, self.address)
